dc37ce8b93e6aba1455ba3a6cb7f6b6ee4b8742c
JoeDeMabior <39078169+JoeDeMabior@users.noreply.github.com>
2019-04-09 22:24:58 +0300

    Create a Settings menu for timer duration

--- app/src/main/java/com/joe/timer/TimerExpiredReceiver.kt
+++ app/src/main/java/com/joe/timer/TimerExpiredReceiver.kt
@@ -3,11 +3,14 @@ package com.joe.timer
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import com.joe.timer.utils.NotificationUtil
 import com.joe.timer.utils.PreferenceUtil
 
 class TimerExpiredReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent) {
+        NotificationUtil.showTimerExpired(context)
+
         PreferenceUtil.setTimerState(MainActivity.TimerState.Stopped, context)
         PreferenceUtil.setAlarmSetTime(0, context)
     }
--- app/src/main/java/com/joe/timer/utils/PreferenceUtil.kt
+++ app/src/main/java/com/joe/timer/utils/PreferenceUtil.kt
@@ -6,9 +6,11 @@ import com.joe.timer.MainActivity
 
 class PreferenceUtil {
     companion object {
+        private const val TIMER_LENGTH_ID = "com.joe.timer.timer_length"
+
         fun getTimerLength(context: Context): Int {
-            // Placeholder
-            return 1
+            val preferences = PreferenceManager.getDefaultSharedPreferences(context)
+            return preferences.getInt(TIMER_LENGTH_ID, 10)
         }
 
         private const val PREVIOUS_TIMER_LENGTH_SECONDS_ID = "com.joe.timer.utils.PREVIOUS_TIMER_LENGTH_SECONDS_ID"
